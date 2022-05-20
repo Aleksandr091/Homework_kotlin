4b005b9d61f22c4c6028b80373a7271b75fd0b25
Александр <chist675@gmail.com>
2022-05-20 11:20:57 +0300

    Дз 10-го,настроил googleMap

--- app/src/main/java/ru/chistov/homework/lesson10/MapsFragment.kt
+++ app/src/main/java/ru/chistov/homework/lesson10/MapsFragment.kt
@@ -1,18 +1,22 @@
 package ru.chistov.homework.lesson10
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Color
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import ru.chistov.homework.R
 import ru.chistov.homework.databinding.FragmentMapsBinding
 import ru.chistov.homework.databinding.FragmentMapsMainBinding
@@ -34,6 +38,7 @@ class MapsFragment : Fragment() {
         _binding = null
     }
 
+    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
         /**
          * Manipulates the map once available.
@@ -44,9 +49,24 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+        map=googleMap
         val moscow = LatLng(55.0, 37.0)
         googleMap.addMarker(MarkerOptions().position(moscow).title("Marker in Moscow"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(moscow))
+        map.setOnMapLongClickListener {
+            addMarkerToArray(it)
+            drawLine()
+        }
+        map.uiSettings.isZoomControlsEnabled = true
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            map.uiSettings.isMyLocationButtonEnabled=true
+            map.isMyLocationEnabled = true
+
+        }
     }
 
     override fun onCreateView(
@@ -64,5 +84,36 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+    private lateinit var map: GoogleMap
+    private val markers: ArrayList<Marker> = arrayListOf()
+    private fun setMarker(
+        location: LatLng,
+        searchText: String,
+        resourceId: Int
+    ): Marker {
+        return map.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title(searchText)
+                .icon(BitmapDescriptorFactory.fromResource(resourceId))
+        )!!
+    }
+
+    private fun addMarkerToArray(location: LatLng) {
+        val marker = setMarker(location, markers.size.toString(), R.drawable.ic_map_pin)
+        markers.add(marker)
+    }
+    private fun drawLine(){
+        var previousBefore: Marker? = null
+        markers.forEach { current->
+            previousBefore?.let{  previous->
+                map.addPolyline(
+                    PolylineOptions().add(previous.position,current.position)
+                        .color(Color.RED)
+                        .width(5f))
+            }
+            previousBefore = current
+        }
     }
 }
